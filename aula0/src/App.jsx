b009bde41dfd0a2cb0bfac290689283c9526cfc3
Higor <higor.a.santos12@hotmail.com>
2022-09-20 17:51:37 -0300

    aula

---
+++ aula0/src/App.jsx
@@ -0,0 +1,11 @@
+import EtiquetasHookForm from "./components/EtiquetasHookForm";
+
+export default function App(){
+
+    return(
+        <>
+            <h1>Aula 08</h1>
+            <EtiquetasHookForm/>
+        </>
+    )
+}
